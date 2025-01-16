7a797faef7e84eb5073b59152070887212319b27
“Aby” <abymicheal@gmail.com>
2025-01-16 10:16:24 +0530

    Commiting new Class to Branch_A

---
+++ src/pack/Local_Class_Aby.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class Local_Class_Aby {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Local Class....");
+	}
+
+}
